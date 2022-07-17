73374d5a41ce6cde672f1f8a6d5a53abd4ff6a85
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-07-17 10:53:46 +0530

    Adds basic tour model and mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,44 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 const app = require('./app');
-console.log(process.env);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => console.log('DB connection successful 🟢🟢🟢'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less than 40 characters'],
+    minlength: [10, 'A tour name must have more than 10 characters'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    max: [5, 'A tour rating must be less than 5'],
+    min: [1, 'A tour rating must be more than 1'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
