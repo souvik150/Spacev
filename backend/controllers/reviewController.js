765b9126c03962f9c13b0e2a43c714955c4f1f24
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-09-19 11:47:39 +0530

    feat: adds handlefactory

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,30 +1,50 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  //
+// const catchAsync = require('./../utils/catchAsync');
 
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
+const factory = require('./handlerFactory');
 
-  const reviews = await Review.find();
+// exports.getAllReviews = catchAsync(async (req, res, next) => {
+//   //
 
-  res.status(200).json({
-    status: 'sucess',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
+//   let filter = {};
+//   if (req.params.tourId) filter = { tour: req.params.tourId };
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
+//   const reviews = await Review.find();
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview
-    }
-  });
-});
+//   res.status(200).json({
+//     status: 'sucess',
+//     results: reviews.length,
+//     data: {
+//       reviews
+//     }
+//   });
+// });
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
+
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   // Allow nested routes
+//   if (!req.body.tour) req.body.tour = req.params.tourId;
+//   if (!req.body.user) req.body.user = req.user.id;
+
+//   const newReview = await Review.create(req.body);
+
+//   res.status(201).json({
+//     status: 'success',
+//     data: {
+//       review: newReview
+//     }
+//   });
+// });
